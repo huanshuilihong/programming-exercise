98819371eee3f013bf3026e4dbb884189a627230
渙水離鴻 <98995126+huanshuilihong@users.noreply.github.com>
2023-01-08 16:42:39 +0800

    Update OutputALineOfNumbersOpposite.java

--- CSC1003 Practice Outline Oct 17 21/OutputALineOfNumbersOpposite.java
+++ CSC1003 Practice Outline Oct 17 21/OutputALineOfNumbersOpposite.java
@@ -1,21 +1,25 @@
-import java.util.*;
 /*
 Read n numbers from the command arguments, and output the in reverse order.
 For example, if you give 5 integers 1 2 3 4 5 as the input, the output should be 5 4 3 2 1.
 You can also use Scanner to get the input from standard input stream, if you want.
+Q.How do I specify the end of the stream?
+A.<Ctrl-d>(standard for decades).
+A.<Ctr1-z>(Windows).
  */
+import java.util.*;
 public class OutputALineOfNumbersOpposite
 {
     public static void main(String[] args)
     {
-Scanner in=new Scanner(System.in);
+        Scanner in=new Scanner(System.in);
         ArrayList<Integer> myArrayList = new ArrayList<>();
         while (in.hasNextInt()){myArrayList.add(in.nextInt());}
         in.close();
         for(int i=myArrayList.size()-1;i>0;i=i-1){
             int a=myArrayList.get(i);
             System.out.print(a+" ");}
-        int a=myArrayList.get(0);
+        int a;
+        a = myArrayList.get(0);
         System.out.print(a);
     }
 }
